b082ae56c1f9be7590cfc155767125e63ce01095
KrzysztofPempera <k.pempera@gmail.com>
2022-05-09 22:06:52 +0200

    Add reference signal generator class

---
+++ BCI_IMF/ReferenceSignal.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class ReferenceSignal():
+    def __init__(self, config):
+        self.referenceSignalType = config.classifierRefSignal
+
+        self.baseSignals = config.baseSignals
+        self.phaseShifts = config.phaseShifts
+
+    def createReferenceSignals(self) -> list:
+        signalCount = len(self.baseSignal)*len(self.phaseShifts)
+        refSignals = []
+
+        for frequency in self.baseSignals:
+            for shift in self.phaseShifts:
+                refSignals.append(self.__createReferenceSignal__(frequency,shift))
+
+        return refSignals
+
+    def __createReferenceSignal__(self, baseSignal, phaseShift) -> float:
+        refSignal = 2 * baseSignal * np.pi + phaseShift*np.pi
--- BCI_IMF/Settings.py
+++ BCI_IMF/Settings.py
@@ -1,14 +1,21 @@
 import json
+import mypy
 
 with open ('appsettings.json', 'r') as para:
     config = json.load(para)
 
 class Config(config):
 
-    def __init__(config):
-        self.classifierType = config['classifier']['type']
-        self.classifierRefSignal = config['classifer']['referenceSignal']
-        self.classifierDataChunk = config['classifer']['dataChunk']
+    def __init__(self, config):
+        self.classifierType:str = config['classifier']['type']
+        self.classifierRefSignal:str = config['classifer']['referenceSignal']
+        self.classifierDataChunk:int = config['classifer']['dataChunk']
 
-        self.baseSignals = config['referenceSignals']['baseSignal']
-        self.phaseShifts = config['referenceSignal']['phaseShift']
+        self.baseSignals:list = config['referenceSignals']['baseSignal']
+        self.phaseShifts:list = config['referenceSignal']['phaseShift']
+
+        self.boardID:int = config['eegBoard']['ID']
+        self.boardPort:str = config['eegBoard']['port']
+
+        self.stimuliCount:int = config['stimuli']['count']
+        self.stimuliColour:str = config['stimuli']['colour']
