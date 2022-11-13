536a304a471f674f954cc548912eca9137031c75
KrzysztofPempera <k.pempera@gmail.com>
2022-11-13 23:20:42 +0100

    c.d.

--- BCI_IMF/screen.py
+++ BCI_IMF/screen.py
@@ -23,10 +23,12 @@ class screen():
         stimuli.setColor([-1,(1 + sin(2 * pi * freq * time + phase_shift*pi))/2,-1])
         stimuli.draw()
 
-    def __set_start__(self, stim1,stim2,stim3,window):
+    def __set_start__(self, stim1,stim2,stim3,stim4,stim5 ,window):
         stim1.draw()
         stim2.draw()
         stim3.draw()
+        stim4.draw()
+        stim5.draw()
 
         window.flip()
 
@@ -37,10 +39,14 @@ class screen():
         stimuli1 = visual.Rect(win = window, size = 200, units = 'pix', fillColor = self.stimuli_colour, pos =(-600, 300))
         stimuli2 = visual.Rect(win = window, size = 200, units = 'pix', fillColor = self.stimuli_colour, pos =(0, 0))
         stimuli3 = visual.Rect(win = window, size = 200, units = 'pix', fillColor = self.stimuli_colour, pos =(600, -300))
+        stimuli4 = visual.Rect(win = window, size = 200, units = 'pix', fillColor = self.stimuli_colour, pos =(600, 300))
+        stimuli5 = visual.Rect(win = window, size = 200, units = 'pix', fillColor = self.stimuli_colour, pos =(-600, -300))
 
-        marker1 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(-600, 0))
-        marker2 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(0, 300))
-        marker3 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(600, 0))
+        marker1 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(-350, 300))
+        marker2 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(200, 0))
+        marker3 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(350, -300))
+        marker4 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(-350, -300))
+        marker5 = visual.Rect(win = window, size = 30, units = 'pix', fillColor = [1,1,1], pos =(350, 300))
 
         welcomeText = visual.TextStim(win = window, text = "Podczas badania wyświetlone zostanie 5 migających kwadratów. Twoim zadaniem będzie skupienie wzroku, na kwardracie oznaczonym przez biały marker. Całość badania trwa 10 minut i jest podzielona na trzy ok. 3minutowe części. Po każdej z cześci nastąpi 3 minutowa przerwa. Naciśnij spację, aby rozpocząć.", height = 20)
         breakText = visual.TextStim(win = window, text = "Przerwa. Aby kontynuować, naciśnij spację.", height = 20)
@@ -75,18 +81,24 @@ class screen():
 
             if start_program.is_set() and breakTime == False:
                 if first == True:
-                    self.__set_start__(stimuli1,stimuli2,stimuli3,window)
+                    self.__set_start__(stimuli1,stimuli2,stimuli3,stimuli4,stimuli5,window)
 
                 self.start_frame += 1
 
                 self.__modulate_stimuli_opacity__(stimuli1, self.start_frame, 7.5, self.screen_freq, 0)
                 self.__modulate_stimuli_opacity__(stimuli2, self.start_frame, 12, self.screen_freq, 0)
                 self.__modulate_stimuli_opacity__(stimuli3, self.start_frame, 14, self.screen_freq, 0)
+                self.__modulate_stimuli_opacity__(stimuli4, self.start_frame, 14, self.screen_freq, 0)
+                self.__modulate_stimuli_opacity__(stimuli5, self.start_frame, 14, self.screen_freq, 0)
 
                 if self.start_frame >= self.screen_freq*5 and self.start_frame <= self.screen_freq*30:
                     if self.start_frame >= self.screen_freq*5 + self.classifier_timeframe*self.screen_freq:
                         current_stimuli.value = 7.5
                     marker1.draw()
+                    marker2.draw()
+                    marker3.draw()
+                    marker4.draw()
+                    marker5.draw()
 
                 elif self.start_frame == self.screen_freq*31:
                     breakTime = True
